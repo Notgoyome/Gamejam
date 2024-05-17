7d1d8656dcf85f2f50bad2fd4fe59d7be57cea6a
Louis-rollet <louis.rollet@epitech.eu>
2024-05-18 01:10:32 +0200

    [ADD] menu fully working transition smooth

--- Menus/Menu.gd
+++ Menus/Menu.gd
@@ -1,5 +1,6 @@
 extends Control
 
+var transition_duration = 0.5
 
 var menu_origin_position = Vector2(0, 0)
 var menu_origin_size = Vector2(0, 0)
@@ -7,7 +8,7 @@ var menu_origin_size = Vector2(0, 0)
 var current_menu
 var menu_stack := []
 
-@onready var menu1: = $Menu1
+@onready var menu1 = $Menu1
 @onready var menu2 = $Menu2
 
 # Called when the node enters the scene tree for the first time.
@@ -16,18 +17,23 @@ func _ready() -> void:
 	menu_origin_size = get_viewport_rect().size
 	current_menu = menu1
 
+
 func move_to_next_menu(next_menu_id: String):
 	var next_menu = get_menu_from_id(next_menu_id)
-	current_menu.position = Vector2(-menu_origin_size.x, 0)
-	next_menu.position = menu_origin_position
+	var tween = get_tree().create_tween()
+	tween.tween_property(current_menu, "position", Vector2(-menu_origin_size.x, 0), transition_duration)
+	tween.tween_property(next_menu, "position", menu_origin_position, transition_duration)
+	tween.play()
 	menu_stack.append(current_menu)
 	current_menu = next_menu
 
 func move_to_previous_menu():
 	var previous_menu = menu_stack.pop_back()
 	if previous_menu != null:
-		previous_menu.position = menu_origin_position
-		current_menu.position = Vector2(menu_origin_size.x, 0)
+		var tween = get_tree().create_tween()
+		tween.tween_property(current_menu, "position", Vector2(menu_origin_size.x, 0), transition_duration)
+		tween.tween_property(previous_menu, "position", menu_origin_position, transition_duration)
+		tween.play()
 		current_menu = previous_menu
 
 func get_menu_from_id(menu_id: String) -> Control:
@@ -39,16 +45,12 @@ func get_menu_from_id(menu_id: String) -> Control:
 		_:
 			return menu1
 
-
-
-
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://Epsilon.tscn")
 
 func _on_options_button_pressed():
-	# get_tree().change_scene_to_file("res://Menus/Options.tscn")
+	# get_tree().change_scene("res://Menus/Options.tscn")
 	move_to_next_menu("menu2")
-
 
 func _on_back_button_pressed():
 	move_to_previous_menu()
@@ -56,10 +58,6 @@ func _on_back_button_pressed():
 func _on_quit_button_pressed():
 	get_tree().quit()
 
-
-func _process(delta):
-	if Input.is_key_pressed(KEY_ESCAPE):
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("ui_cancel"):
 		get_tree().quit()
-
-
-	
