54d2f460915c80d8b8073e249a32c8d8cc0e6ea0
Guillame Tran <guillaume1.tran@epitech.eu>
2024-05-18 14:36:10 +0200

    first scene

---
+++ script/pipe.gd
@@ -0,0 +1,49 @@
+extends TileMap
+
+@onready var timer = $Timer
+
+var coords_list = [[4,5],[7,5],[7,9], [4,9]]
+var output_list = [[coords_list[0], coords_list[1]],
+				   [coords_list[1], coords_list[2]],
+				   [coords_list[2], coords_list[3]],
+				   [coords_list[3], coords_list[0]]]
+var pipe_output_list = [[4,8],[5,5],[7,6],[6,9]]
+var pipe_input : Vector2i = Vector2i(1, 7)
+
+# Called when the node enters sthe scene tree for the first time.
+func _ready():
+	timer.start()
+	pass # Replace with function body.
+
+func spread_gas():
+	#look for pipe input in the map
+	var input_coords = get_cell_atlas_coords(0, pipe_input)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	spread_gas()
+	pass
+
+func get_next_rotation(atlas_coord):
+	for i in range(0, 4):
+		if atlas_coord == Vector2i(coords_list[i][0], coords_list[i][1]):
+			return Vector2i(coords_list[(i+1)%4][0], coords_list[(i+1)%4][1])
+	return Vector2i(-1, -1)
+
+func _on_timer_timeout():
+    # Itérer sur toutes les cellules du TileMap et appliquer la rotation
+	print("test")
+    
+    # Example coordinates for a 2x2 region
+	var start_coords = Vector2i(12, 29)
+	var end_coords = Vector2i(12, 29) # Inclusive range
+
+	for x in range(start_coords.x, end_coords.x + 1):
+			var coords = Vector2i(x, start_coords.y)
+			var atlas_coords = get_cell_atlas_coords(0, coords)
+			print("atlas cord: ", atlas_coords)
+			if atlas_coords != Vector2i(-1, -1) and [atlas_coords.x, atlas_coords.y] in coords_list:
+				var new_atlas_coords = get_next_rotation(atlas_coords)
+				set_cell(0, coords, 0, new_atlas_coords)
+
+	pass # Replace with function body.
