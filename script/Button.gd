1736f6431b316187463a3dcd1956799e7114bbc6
Guillame Tran <guillaume1.tran@epitech.eu>
2024-05-19 14:07:58 +0200

    interactable pipe

--- script/Button.gd
+++ script/Button.gd
@@ -3,6 +3,7 @@ extends StaticBody2D
 @onready var interaction_area = $InteractionArea
 @onready var sprite = $AnimatedSprite2D
 @onready var timer = $Timer
+@export var pipe: Pipe
 
 var _activated: bool = false
 
@@ -20,8 +21,12 @@ func _ready():
 
 func _toggle_lamp():
 	activated = true
+	if pipe:
+		pipe.WORKING = true
 	timer.start(1.0)
 
 func _turn_off_lamp():
 	activated = false
+	if pipe:
+		pipe.WORKING = false
 	timer.stop()
